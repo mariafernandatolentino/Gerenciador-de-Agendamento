ed037d8d8410b31e2a87b45db9c454c38c632ae0
mariafernanda_tolentino <mariafernandartolentino@gmail.com>
2023-11-02 15:10:24 -0300

    Gerenciador de Agendamento final

--- server.js
+++ server.js
@@ -1,71 +1,69 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
-const db = require('./database');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
 
 const app = express();
+const porta = 3030;
+
+const db = new sqlite3.Database('consultorio.db');
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS consultas (
+      id INTEGER PRIMARY KEY, 
+      data TEXT, 
+      hora TEXT, 
+      paciente TEXT, 
+      medico TEXT, 
+      status TEXT
+  )
+`);
 
 app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-// Agendamento de consulta
-app.post('/agendar', [
-  check('data').isDate(),
-  check('hora').isLength({ min: 5, max: 5 }),
-  check('paciente').isLength({ min: 1 }),
-  check('medico').isLength({ min: 1 }),
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
+// Agendando uma consulta 
+app.post('/agendar', (req, res) => {
   const { data, hora, paciente, medico } = req.body;
 
-  // Insira a consulta no banco de dados
+// Insirindo uma consulta no banco de dados
   db.run(
     'INSERT INTO consultas (data, hora, paciente, medico, status) VALUES (?, ?, ?, ?, ?)',
     [data, hora, paciente, medico, 'marcado'],
-    (err) => {
-      if (err) {
-        console.error('Erro ao agendar consulta:', err.message);
-        return res.status(500).send('Erro interno do servidor');
+    (erro) => {
+      if (erro) {
+        return res.status(500).json({ erro: erro.message });
       }
       res.status(201).json({ message: 'Consulta agendada com sucesso' });
     }
   );
 });
 
-// Listagem de consultas agendadas
+// Listando as consultas agendadas
 app.get('/consultas', (req, res) => {
-  db.all('SELECT * FROM consultas', (err, rows) => {
-    if (err) {
-      console.error('Erro ao buscar consultas:', err.message);
-      return res.status(500).send('Erro interno do servidor');
+  db.all('SELECT * FROM consultas', (erro, resultados) => {
+    if (erro) {
+      return res.status(500).json({ erro: erro.message });
     }
-    res.json(rows);
+    res.json(resultados);
   });
 });
 
-// Cancelamento de consulta
-app.delete('/cancelar/:id', (req, res) => {
+// Atualizando o status da consulta para "cancelado"
+app.put('/cancelar/:id', (req, res) => {
   const consultaId = req.params.id;
 
-
-  // Atualize o status da consulta para "cancelado"
   db.run(
     'UPDATE consultas SET status = ? WHERE id = ?',
     ['cancelado', consultaId],
-    (err) => {
-      if (err) {
-        console.error('Erro ao cancelar consulta:', err.message);
-        return res.status(500).send('Erro interno do servidor');
+    (erro) => {
+      if (erro) {
+        return res.status(500).json({ erro: erro.message });
       }
       res.json({ message: 'Consulta cancelada com sucesso' });
     }
   );
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando em localhost:${porta}`);
 });
